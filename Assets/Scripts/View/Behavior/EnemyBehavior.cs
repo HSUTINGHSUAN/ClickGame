a0fd0ff6fc6cfb733ecc9cbb8368713fee1aefce
4419-1-4\user <f5200225@gmail.com>
2017-12-23 12:00:53 +0800

    1.完成healthComponent

--- Assets/Scripts/View/Behavior/EnemyBehavior.cs
+++ Assets/Scripts/View/Behavior/EnemyBehavior.cs
@@ -1,24 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[RequireComponent(typeof(HealthComponent))]
 [RequireComponent(typeof(Animator))]//attribute//強制限制這個class一定要掛Animator才能使用
-
+[RequireComponent(typeof(MeshFader))]
+[RequireComponent(typeof(AudioSource))]
 public class EnemyBehavior : MonoBehaviour {
     private Animator animator;//指標
-
+    private MeshFader meshFader;
+    private AudioSource audioSource;
+    private HealthComponent healthComponent;
+    public AudioClip hurtClip;
     private void Awake()
     {
         animator = GetComponent<Animator>();//抓物件
-        //animator.SetTrigger("die");
+        meshFader = GetComponent<MeshFader>();
+        audioSource = GetComponent<AudioSource>();
+        healthComponent = GetComponent<HealthComponent>();
     }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnEnable()
+    {
+        StartCoroutine(meshFader.FadeIn());
+        healthComponent.Init(100);
+    }
+
+    public void DoDamage(int attack)
+    {
+        animator.SetTrigger("hurt");
+        audioSource.clip = hurtClip;
+        audioSource.Play();
+        healthComponent.Hurt(attack);
+    }
+    private void Update()
+    {
+        if (healthComponent.IsOver)
+            return;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            DoDamage(10);
+
+        }
+    }
 }
---
+++ Assets/Scripts/View/Component/HealthComponent.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthComponent : MonoBehaviour {
+    [SerializeField]//private Slider healthSlider就算是私有的也讓其他人能夠拖曳使用
+    private Slider healthSlider;
+    private int currentHealth;
+    //-------封裝性↑
+    [SerializeField]
+    private float speed = 5f;
+
+
+    public bool IsOver
+    {
+        get
+        {
+            return currentHealth <= healthSlider.minValue;
+        }
+    }
+
+    [ContextMenu("Test Init 100")]
+    private void TestInit()
+    {
+        Init(100);
+    }
+    [ContextMenu("Test Hurt 50")]
+    private void TestHurt()
+    {
+        Hurt(50);
+    }
+
+    public void Init(int maxHealth)
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = maxHealth;
+        currentHealth = maxHealth;
+    }
+    public void Hurt(int damage)
+    {
+        currentHealth -= damage;
+        currentHealth = (int)Mathf.Max(currentHealth, healthSlider.minValue);//會回傳最大值
+    }
+
+    private void Update()
+    {
+        healthSlider.value = Mathf.Lerp(healthSlider.value, currentHealth, Time.deltaTime * speed);
+    }
+
+}
