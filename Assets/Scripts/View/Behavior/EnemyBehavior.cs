54ec8bf58cdbd62d47d6a0386fe4b58af1c187fd
4419-1-4\user <f5200225@gmail.com>
2017-12-23 10:55:26 +0800

    1.新增EnemyBehavior 2.完成meshFader

---
+++ Assets/Scripts/View/Behavior/EnemyBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]//attribute//強制限制這個class一定要掛Animator才能使用
+
+public class EnemyBehavior : MonoBehaviour {
+    private Animator animator;//指標
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();//抓物件
+        //animator.SetTrigger("die");
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
